98982800997ac8b005a4b2e7a0cc46a5bd5aa945
MarcinKalinowskiJS <marcinkalinowski5@o2.pl>
2020-07-11 12:31:17 +0200

    1000 belt objects working under coroutines and showing estimated FPS

---
+++ Assets/Belt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Belt
+{
+    Belt input, output;
+    int? resource;
+    float speed;
+    float lastTransferTime;
+    bool working = true;
+
+    public Belt(Belt cInput, Belt cOutput, int? cResource, float cSpeed) {
+        input = cInput;
+        output = cOutput;
+        resource = cResource;
+        speed = cSpeed;
+    }
+
+    public IEnumerator transfer() {
+        while (working)
+        {
+            if (input != null) 
+            {
+                resource = (int?)this.transfer().Current;
+                    BeltTest.transfers++;
+            }
+
+            yield return resource;
+        }
+    }
+
+    public void setInput(Belt mInput) {
+        input = mInput;
+    }
+
+    public void setOutput(Belt mOutput)
+    {
+        output = mOutput;
+    }
+
+    public void setResource(int? mResource) {
+        resource = mResource;
+    }
+}
---
+++ Assets/BeltTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltTest : MonoBehaviour
+{
+    public static long transfers = 0;
+    public int maxResourceNumber=0;
+    float tmpTimeInsert = 0, tmpTimeDebug = 0;
+    Belt ending, beggining;
+    bool testingBelt = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (testingBelt == true)
+        {
+            testBelt();
+        }
+        
+    }
+
+    public void testBelt() { 
+    Belt inTmp = null, outTmp;
+    outTmp = ending = new Belt(null, null, maxResourceNumber++, 0.2f);
+        for (int i = 0; i< 1000; i++) {
+            inTmp = new Belt(null, outTmp, maxResourceNumber++, 0.25f);
+            outTmp.setInput(inTmp);
+            StartCoroutine(outTmp.transfer());
+            outTmp = inTmp;
+        }
+        if (inTmp != null)
+        {
+            beggining = inTmp;
+            StartCoroutine(inTmp.transfer());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (testingBelt == true)
+        {
+            tmpTimeInsert += Time.deltaTime;
+            tmpTimeDebug += Time.deltaTime;
+            if (tmpTimeInsert > 0.1f)
+            {
+                tmpTimeInsert = 0;
+                beggining.setResource(maxResourceNumber++);
+            }
+            if (tmpTimeDebug > 0.5f) {
+                tmpTimeDebug = 0;
+                Debug.Log("Max resource number: " + maxResourceNumber + " \tTransfers: " + transfers);
+            }
+            
+        }
+    }
+}
